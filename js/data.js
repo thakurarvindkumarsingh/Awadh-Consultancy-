c17d7e41b7af41a944e8c568075a40a4677f777c
thakurarvindkumarsingh <arvindkumarsingh1170@gmail.com>
2026-08-26 17:11:16 +0530

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,93 @@
+const governmentServices=[
+
+{
+id:1,
+title:"eGram Swaraj",
+category:"panchayat",
+icon:"fa-building-columns",
+description:"Official Panchayat Governance Portal.",
+url:"https://egramswaraj.gov.in"
+},
+
+{
+id:2,
+title:"Gram Manchitra",
+category:"panchayat",
+icon:"fa-map-location-dot",
+description:"Village GIS Mapping Portal.",
+url:"https://grammanchitra.gov.in"
+},
+
+{
+id:3,
+title:"PM Kisan",
+category:"agriculture",
+icon:"fa-seedling",
+description:"Farmer Benefit Scheme Portal.",
+url:"https://pmkisan.gov.in"
+},
+
+{
+id:4,
+title:"UIDAI Aadhaar",
+category:"identity",
+icon:"fa-id-card",
+description:"Aadhaar Services Portal.",
+url:"https://uidai.gov.in"
+},
+
+{
+id:5,
+title:"ABHA Health ID",
+category:"health",
+icon:"fa-heart-pulse",
+description:"Digital Health ID Services.",
+url:"https://abdm.gov.in"
+},
+
+{
+id:6,
+title:"UP Scholarship",
+category:"education",
+icon:"fa-graduation-cap",
+description:"Scholarship Portal for Students.",
+url:"#"
+},
+
+{
+id:7,
+title:"NREGA Job Card",
+category:"employment",
+icon:"fa-briefcase",
+description:"Employment Guarantee Portal.",
+url:"#"
+},
+
+{
+id:8,
+title:"Electricity Bill",
+category:"utilities",
+icon:"fa-bolt",
+description:"Pay Electricity Bills Online.",
+url:"#"
+},
+
+{
+id:9,
+title:"Jan Dhan Yojana",
+category:"utilities",
+icon:"fa-wallet",
+description:"Banking Services Portal.",
+url:"#"
+},
+
+{
+id:10,
+title:"CSC Login",
+category:"utilities",
+icon:"fa-desktop",
+description:"Common Service Centre Login.",
+url:"#"
+}
+
+];
